b1e290d16ef0e0516dce63300fc201345a654caf
AsathKhan <asath2k2@gmail.com>
2024-06-01 08:23:40 +0530

    updated css for catlog table, add product

--- customerapp/urls.py
+++ customerapp/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage, name="homepageurl"),
-    path('Catlog/addproducts', views.products, name="addproductsurl"),
-    path('Catlog', views.ptable, name="catlogurl"),
+    path('CatalogList/Addproducts', views.products, name="addproductsurl"),
+    path('CatalogList', views.ptable, name="catlogurl"),
     path('Update/<int:id>',views.update, name= "updateurl"),
     path('Delete/<int:id>', views.delete, name= "deleteurl"),
     path('employee',views.employee, name = 'employeeurl'),
     path('About', views.about, name='abouturl'),
-    path('Catlogg', views.ptable, name="catloggurl"),   
-    path('ViewProducts', views.viewproducts, name = "viewproductsurl"),
+    path('CatalogTable', views.ptable, name="catloggurl"),   
+    path('Catalog', views.viewproducts, name = "viewproductsurl"),
 ]
